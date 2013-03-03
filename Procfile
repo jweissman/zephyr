19459a1ac4ff68ad63c80386de3411cd2151c354
jweissman <joseph.weissman@tapjoy.com>
2013-03-03 16:24:36 -0500

    Update: use secondary faye app

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web:     rails server -p $PORT
-faye:    bundle exec rackup faye.ru -s thin -E production
 #worker: bundle exec rake jobs:work
--- config/initializers/backbone_sync_rails_faye.rb
+++ config/initializers/backbone_sync_rails_faye.rb
@@ -1,2 +1,2 @@
-faye_host = "localhost"
-BackboneSync::Rails::Faye.root_address = "http://#{faye_host}:9292"
+faye_host = Rails.env.production? ? "zephyr-faye.herokuapp.com" : "localhost:9292"
+BackboneSync::Rails::Faye.root_address = "http://#{faye_host}"
