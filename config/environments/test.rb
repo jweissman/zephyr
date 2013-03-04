b5673fdc789b0c5bccdbc2436bc5e085db4e9b62
jweissman <joseph.weissman@tapjoy.com>
2013-03-04 18:12:52 -0500

    Update: fixup email settings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,7 +35,7 @@ Zephyr::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
-  config.action_mailer.default_url_options = { host: 'example.com' }
+  config.action_mailer.default_url_options = { host: 'zephyr-games.herokuapp.com' }
 end
 
 # Turn down the cost so that specs run quickly.
--- config/initializers/smtp_gmail.rb
+++ config/initializers/smtp_gmail.rb
@@ -1,10 +1,13 @@
 ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.raise_delivery_errors = true
+#ActionMailer::Base.perform_deliveries = !Rails.env.test?
+
 ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address              => "smtp.gmail.com",
+    :address              => 'smtp.gmail.com',
     :port                 => 587,
-    :authentication       => :plain,
-    :domain               => 'zephyr-games.heroku.com',
+    :domain               => 'gmail.com',
     :user_name            => ENV['GMAIL_SMTP_USER'],
-    :password             => ENV['GMAIL_SMTP_PASSWORD']
+    :password             => ENV['GMAIL_SMTP_PASSWORD'],
+    :authentication       => 'login',
+    :enable_starttls_auto => true
 }
