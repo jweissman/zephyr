b8f0f1da3be7b8c2acde38669c94bbeec7a2bee6
jweissman <joseph.weissman@tapjoy.com>
2013-03-04 18:12:52 -0500

    Update: fixup email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Zephyr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
